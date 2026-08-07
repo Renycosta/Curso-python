3ae2841af0d22540cbdd95fea1560e9e03825505
Reny <reny.dev03@gmail.com>
2026-08-06 22:07:43 -0300

    Aula03

---
+++ aula03.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programção
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print('Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
---
+++ aula04.py
@@ -0,0 +1,21 @@
+# Tipos int e float
+# int -> Número inteiro 
+# O tipo int representa qualquer número
+# postivo ou negativo. intsem sinal é considerado 
+# positivo
+print(11) # int
+print(-11) # int
+print(0) # int
+
+# Float -> Número com ponto flutuante
+# O tipo float representa qualquer número
+# positivo ou negativo com ponto flutuante
+# float sem sinal é considerado positivo
+print(10.1) # float
+print(0.0, -1.5) # float
+
+# A função type mostra o tipo que o Python
+# inferiu ao valor
+print(type('Otávio'))
+print(type(1))
+print(type(1.2))
