3a25ce6fa45d9bfa29e6ef0548ea2cc30a3a6fb5
Reny <reny.dev03@gmail.com>
2026-08-08 12:05:22 -0300

    Aula12

---
+++ aula12.py
@@ -0,0 +1,7 @@
+nome = "Luiz Otávio" 
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+resultado = f"{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc}"
+
+print(resultado)
