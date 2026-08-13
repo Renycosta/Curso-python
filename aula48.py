73f38c136d16f236a00d2f6277eff8a479526f87
Reny <reny.dev03@gmail.com>
2026-08-13 17:42:46 -0300

    Aula48

---
+++ aula48.py
@@ -0,0 +1,18 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+#        +01234
+#        -54321
+string = "ABCDE"    # 5
+lista = [123, True, "Luiz Otávio", 1.2, []]
+lista[2] = "Maria" 
+
+print(lista)
+print(lista[2], type(lista[2]))
+
+# print(bool([])) # falsy
+# print(lista, type(lista))
