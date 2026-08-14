26028a1a63f4459c2599cab21429aad938f83c41
Reny <reny.dev03@gmail.com>
2026-08-14 14:23:48 -0300

    Aula57

---
+++ aula57.py
@@ -0,0 +1,20 @@
+"""
+enumerate - enumera iteráveis (índices)
+"""
+# [(0, 'Maria'), (1, 'Helena'), (2, 'Luiz'), (3, 'João')]
+lista = ["Maria", "Helena", "Luiz"]
+lista.append("João")
+
+lista_enumerada = list(enumerate(lista))
+print(lista_enumerada)
+
+for item in enumerate(lista):
+    print(item)
+
+for indice, nome  in enumerate(lista):
+    print(indice, nome)
+
+for tupla_enumerada in enumerate(lista):
+    print("FOR da tupla:")
+    for valor in tupla_enumerada:
+        print(f"\t{valor}")
