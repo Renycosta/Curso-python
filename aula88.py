ceef68a70f586725b0b7624600ffa8580b3706c3
Reny <reny.dev03@gmail.com>
2026-08-18 21:12:26 -0300

    Aula88

---
+++ aula88.py
@@ -0,0 +1,12 @@
+# Exemplo de uso de sets
+letras = set()
+
+while True:
+    letra = input("Digite: ")
+    letras.add(letra.lower())
+
+    if "l" in letras:
+        print("PARABÉNS")
+        break
+
+    print(letras)
