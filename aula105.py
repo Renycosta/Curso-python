cc34d2e2998c7e5e253dc026c48908528f3e1a1b
Reny <reny.dev03@gmail.com>
2026-08-20 16:19:17 -0300

    Aula105

---
+++ aula105.py
@@ -0,0 +1,25 @@
+# Try, except, else e finally
+
+try:
+    a = 18
+    b = 0
+    print("Linha 1")
+    c = a / b
+    print("Linha 2")
+
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
+    print(e)
+
+except NameError:
+    print("Nome b não está definido")
+
+except (TypeError, IndexError) as error:
+    print("TypeError + IndexError")
+    print("MSG:", error)
+    print("Nome:", error.__class__.__name__)
+
+except Exception:
+    print("Erro desconhecido")
+
+print("Continuar")
