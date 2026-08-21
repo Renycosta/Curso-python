2f9bc98df6c15fdfc5667fcb633d327e69752b02
Reny <reny.dev03@gmail.com>
2026-08-21 17:04:03 -0300

    Aula111

---
+++ aula111.py
@@ -0,0 +1,9 @@
+import aula111_package.modulo
+from aula111_package import modulo
+from aula111_package.modulo import soma_do_modulo
+from aula111_package.modulo import *
+
+print(aula111_package.modulo.soma_do_modulo(1, 2))
+print(modulo.soma_do_modulo(1, 2))
+print(soma_do_modulo(1, 2))
+print(variavel)
---
+++ aula111_package/modulo.py
@@ -0,0 +1,11 @@
+__all__ = [
+    "variavel",
+    "soma_do_modulo"
+]
+
+variavel = "Alguma coisa"
+
+def soma_do_modulo(x, y):
+    return x + y
+
+nova_variavel = "Ok"
