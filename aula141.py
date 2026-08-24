3d7d15278e3458e48fdcaa7efba3ac0ee888aaf4
Reny <reny.dev03@gmail.com>
2026-08-24 17:23:50 -0300

    Aula141

---
+++ aula141.py
@@ -0,0 +1,55 @@
+# Exercício - Lista de tarefas com desfazer e refazer
+# todo = [] -> lista de tarefas
+# todo = ['fazer café'] -> Adicionar fazer café
+# todo = ['fazer café', 'caminhar'] -> Adicionar caminhar
+# desfazer = ['fazer café',] -> Refazer ['caminhar']
+# desfazer = [] -> Refazer ['caminhar', 'fazer café']
+# refazer = todo ['fazer café']
+# refazer = todo ['fazer café', 'caminhar']
+
+todo = []
+
+desfeito = []
+
+def listar():
+    print("TAREFAS:")
+    for i in todo:
+        print(i)
+    print()
+
+def verificacao_desfazer():
+    if todo:
+        return desfazer()
+    return print("Nada a desfazer\n")
+
+def desfazer():
+    des = todo.pop()
+    desfeito.append(des)
+    listar()
+
+def verificacao_refazer():
+    if desfeito:
+        return refazer()
+    return print("Nada a refazer\n")
+
+def refazer():
+    todo.append(desfeito[-1])
+    desfeito.pop()
+    listar()
+
+def adicionar(tarefa):
+    todo.append(tarefa)
+    listar()
+
+while True:
+    print("Comandos: listar, desfazer, refazer")
+    comando_usu  = input("Digite uma tarefa ou comando: ")
+
+    if comando_usu == "listar":
+        listar()
+    elif comando_usu == "desfazer":
+        verificacao_desfazer()
+    elif comando_usu == "refazer":
+        verificacao_refazer()
+    else:
+        adicionar(comando_usu)
